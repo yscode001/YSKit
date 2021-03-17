f4f2c9c26cee5ef9c19497e8d2b99845f0a80422
ys <yscode@126.com>
2021-03-17 13:20:12 +0800

    codable property wrapper

--- YSKitProject/YSKitProject/CodableDefaultValue/CodableDefaultValue.swift
+++ YSKitProject/YSKitProject/CodableDefaultValue/CodableDefaultValue.swift
@@ -6,23 +6,35 @@
 //
 
 // MARK: - 默认值协议
-protocol DefaultValueProtocol {
+public protocol CodableDefaultValueProtocol {
     
-    associatedtype DefaultValueType: Decodable
+    associatedtype DefaultValueType: Codable
     
     /// 默认值
     static var defaultValue: DefaultValueType { get }
 }
 
 // MARK: - propertyWrapper
-
 @propertyWrapper
-struct Default<T: DefaultValueProtocol> {
+struct CodableDefault<T: CodableDefaultValueProtocol> where T == T.DefaultValueType {
     
     var wrappedValue: T.DefaultValueType
+    
+    init(_ value: T.DefaultValueType? = nil) {
+        if let value = value {
+            wrappedValue = value
+        } else {
+            wrappedValue = T.defaultValue
+        }
+    }
 }
 
-extension Default: Decodable {
+extension CodableDefault: Decodable {
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try? container.encode(wrappedValue)
+    }
     
     init(from decoder: Decoder) throws {
         if let container = try? decoder.singleValueContainer(), let val = try? container.decode(T.DefaultValueType.self){
@@ -36,98 +48,67 @@ extension Default: Decodable {
 // MARK: - 重写KeyedDecodingContainer对于Default类型解码的实现
 extension KeyedDecodingContainer {
     
-    func decode<T>(_ type: Default<T>.Type, forKey key: Key) throws -> Default<T> where T: DefaultValueProtocol{
-        try decodeIfPresent(type, forKey: key) ?? Default(wrappedValue: T.defaultValue)
+    func decode<T>(_ type: CodableDefault<T>.Type, forKey key: Key) throws -> CodableDefault<T> where T: CodableDefaultValueProtocol{
+        try decodeIfPresent(type, forKey: key) ?? CodableDefault(T.defaultValue)
     }
 }
 
 // MARK: - 具体类型需遵守DefaultValue协议
-extension Int{
-    enum Int_0: DefaultValueProtocol{ static let defaultValue = 0 }
+extension Int: CodableDefaultValueProtocol{
+    public static var defaultValue: Int{ return 0 }
 }
-extension Int8{
-    enum Int8_0: DefaultValueProtocol{ static let defaultValue = 0 }
+extension Int8: CodableDefaultValueProtocol{
+    public static var defaultValue: Int8{ return 0 }
 }
-extension Int16{
-    enum Int16_0: DefaultValueProtocol{ static let defaultValue = 0 }
+extension Int16: CodableDefaultValueProtocol{
+    public static var defaultValue: Int16{ return 0 }
 }
-extension Int32{
-    enum Int32_0: DefaultValueProtocol{ static let defaultValue = 0 }
+extension Int32: CodableDefaultValueProtocol{
+    public static var defaultValue: Int32{ return 0 }
 }
-extension Int64{
-    enum Int64_0: DefaultValueProtocol{ static let defaultValue = 0 }
-}
-
-extension UInt{
-    enum UInt_0: DefaultValueProtocol{ static let defaultValue = 0 }
-}
-extension UInt8{
-    enum UInt8_0: DefaultValueProtocol{ static let defaultValue = 0 }
-}
-extension UInt16{
-    enum UInt16_0: DefaultValueProtocol{ static let defaultValue = 0 }
-}
-extension UInt32{
-    enum UInt32_0: DefaultValueProtocol{ static let defaultValue = 0 }
-}
-extension UInt64{
-    enum UInt64_0: DefaultValueProtocol{ static let defaultValue = 0 }
+extension Int64: CodableDefaultValueProtocol{
+    public static var defaultValue: Int64{ return 0 }
 }
 
-extension Float{
-    enum Float_0: DefaultValueProtocol{ static let defaultValue = 0 }
+extension UInt: CodableDefaultValueProtocol{
+    public static var defaultValue: UInt{ return 0 }
 }
-extension Float32{
-    enum Float32_0: DefaultValueProtocol{ static let defaultValue = 0 }
+extension UInt8: CodableDefaultValueProtocol{
+    public static var defaultValue: UInt8{ return 0 }
 }
-extension Float64{
-    enum Float64_0: DefaultValueProtocol{ static let defaultValue = 0 }
+extension UInt16: CodableDefaultValueProtocol{
+    public static var defaultValue: UInt16{ return 0 }
 }
-extension Float80{
-    enum Float80_0: DefaultValueProtocol{ static let defaultValue = 0 }
+extension UInt32: CodableDefaultValueProtocol{
+    public static var defaultValue: UInt32{ return 0 }
 }
-
-extension Double{
-    enum Double_0: DefaultValueProtocol{ static let defaultValue = 0 }
-}
-
-extension Bool{
-    enum False: DefaultValueProtocol { static let defaultValue = false }
-    enum True: DefaultValueProtocol { static let defaultValue = true }
+extension UInt64: CodableDefaultValueProtocol{
+    public static var defaultValue: UInt64{ return 0 }
 }
 
-extension String{
-    enum Empty: DefaultValueProtocol { static let defaultValue = "" }
+extension Float: CodableDefaultValueProtocol{
+    public static var defaultValue: Float{ return 0 }
 }
 
-extension Date{
-    enum now: DefaultValueProtocol { static let defaultValue = Date() }
+extension Double: CodableDefaultValueProtocol{
+    public static var defaultValue: Double{ return 0 }
 }
 
+extension Bool: CodableDefaultValueProtocol{
+    public static var defaultValue: Bool{ return false }
+}
 
-// MARK: - 扩展Default
-extension Default{
-    typealias Int_0 = Default<Int.Int_0>
-    typealias Int8_0 = Default<Int8.Int8_0>
-    typealias Int16_0 = Default<Int16.Int16_0>
-    typealias Int32_0 = Default<Int32.Int32_0>
-    typealias Int64_0 = Default<Int64.Int64_0>
-    
-    typealias UInt_0 = Default<UInt.UInt_0>
-    typealias UInt8_0 = Default<UInt8.UInt8_0>
-    typealias UInt16_0 = Default<UInt16.UInt16_0>
-    typealias UInt32_0 = Default<UInt32.UInt32_0>
-    typealias UInt64_0 = Default<UInt64.UInt64_0>
-    
-    typealias Float_0 = Default<Float.Float_0>
-    typealias Float32_0 = Default<Float32.Float32_0>
-    typealias Float64_0 = Default<Float64.Float64_0>
-    typealias Float80_0 = Default<Float80.Float80_0>
-    
-    typealias Double_0 = Default<Double.Double_0>
-    
-    typealias True = Default<Bool.True>
-    typealias False = Default<Bool.False>
-    typealias EmptyString = Default<String.Empty>
-    typealias now = Default<Date.now>
+extension String: CodableDefaultValueProtocol{
+    public static var defaultValue: String{ return "" }
+}
+
+extension Date: CodableDefaultValueProtocol{
+    public static var defaultValue: Date{ return Date() }
+}
+
+extension Array: CodableDefaultValueProtocol where Element: CodableDefaultValueProtocol, Element: Codable {
+    public typealias Value = Array<Element>
+    public static var defaultValue: Array<Element> {
+        return []
+    }
 }

6c315152e456f11e61cce8fb2ddaee030c4f3227
ys <yscode@126.com>
2023-01-17 09:42:00 +0800

    完善

--- YSKitProject/YSKit/Extension/Foundation/NSCoder+ext.swift
+++ YSKitProject/YSKit/Extension/Foundation/NSCoder+ext.swift
@@ -7,67 +7,67 @@
 
 public extension YSOriginalProtocol where OriginalType: NSCoder{
     
-    func decodeString(forKey key: String) -> String{
+    func decodeString(forKey key: String, `default` value: String = "") -> String{
         if originalObj.containsValue(forKey: key){
-            return (originalObj.decodeObject(forKey: key) as? String) ?? ""
+            return (originalObj.decodeObject(forKey: key) as? String) ?? value
         } else{
-            return ""
+            return value
         }
     }
     
-    func decodeBool(forKey key: String) -> Bool{
+    func decodeBool(forKey key: String, `default` value: Bool = false) -> Bool{
         if originalObj.containsValue(forKey: key){
             return originalObj.decodeBool(forKey: key)
         } else{
-            return false
+            return value
         }
     }
     
-    func decodeCInt(forKey key: String) -> Int32{
+    func decodeCInt(forKey key: String, `default` value: CInt = 0) -> CInt{
         if originalObj.containsValue(forKey: key){
             return originalObj.decodeCInt(forKey: key)
         } else{
-            return 0
+            return value
         }
     }
     
-    func decodeInt32(forKey key: String) -> Int32{
+    func decodeInt32(forKey key: String, `default` value: Int32 = 0) -> Int32{
         if originalObj.containsValue(forKey: key){
             return originalObj.decodeInt32(forKey: key)
         } else{
-            return 0
+            return value
         }
     }
     
-    func decodeInt64(forKey key: String) -> Int64{
+    func decodeInt64(forKey key: String, `default` value: Int64 = 0) -> Int64{
         if originalObj.containsValue(forKey: key){
             return originalObj.decodeInt64(forKey: key)
         } else{
-            return 0
+            return value
         }
     }
     
-    func decodeFloat(forKey key: String) -> Float{
+    func decodeFloat(forKey key: String, `default` value: Float = 0) -> Float{
         if originalObj.containsValue(forKey: key){
             return originalObj.decodeFloat(forKey: key)
         } else{
-            return 0
+            return value
         }
     }
     
-    func decodeDouble(forKey key: String) -> Double{
+    func decodeDouble(forKey key: String, `default` value: Double = 0) -> Double{
         if originalObj.containsValue(forKey: key){
             return originalObj.decodeDouble(forKey: key)
         } else{
-            return 0
+            return value
         }
     }
     
-    func decodeInteger(forKey key: String) -> Int{
+    func decodeInteger(forKey key: String, `default` value: Int = 0) -> Int{
         if originalObj.containsValue(forKey: key){
             return originalObj.decodeInteger(forKey: key)
         } else{
-            return 0
+            return value
         }
     }
 }
